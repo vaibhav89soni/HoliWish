a38bf5d8d7626f7baf660abf3eb86236c69b8a05
Vaibhav Soni <soni89vaibhav@gmail.com>
2023-03-04 00:33:42 +0530

    commit
    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let username;
+
+// console.log(username);
+
+//Div 2 is hidden 
+document.getElementById("div2").style.display = "none";
+
+//On click of Back Button Div 2 will be displayed
+document.getElementById("submitVal1").onclick = function(){
+    username=document.getElementById("text1").value;
+    // console.log(username);
+    document.getElementById("div1").style.display = "none"; //div1 will be hidden on submit of value
+    if(document.getElementById("div2").style.display == "none"){
+        document.getElementById("div2").style.display="block";
+    }
+    // document.getElementById("lable2").innerHTML=username+ ", बोलो जय माता की...";
+    document.getElementById("lable2").innerHTML=username+ ", ...आओ, होली खेलें";
+    // console.log(username);
+}
+
+//On click of Back Button Div 1 will be displayed
+document.getElementById("submitVal2").onclick = function(){
+    if(document.getElementById("div1").style.display == "none"){
+        document.getElementById("div1").style.display="block";
+    }
+    document.getElementById("div2").style.display = "none";
+}
+
